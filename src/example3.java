8708fc5594b9d34d11cfa077db11c1a3132774e7
Sap1ent <christopher.west@hotmail.co.uk>
2020-08-24 23:18:53 +0100

    Initial commit failed so updated GIT on local machine and attempting again

---
+++ src/example3.java
@@ -0,0 +1,14 @@
+/* This program illustrates the difference
+ between int and double
+ */
+
+
+class example3 {
+    public static void main (String args[]) {}
+        int var; //this declares and int variable
+        double x; //this declares a floating-point variable
+
+        var = 10; // assign the var a value of 10
+
+        x = 10.0 // assign x the value of 10.0
+}
